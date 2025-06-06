9eadeb67d1e14f0cfd0d151209353be29737b439
Ranko1996 <koturic96@gmail.com>
2025-06-06 12:17:20 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,78 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+
+const jwt = require('jsonwebtoken');
+
+app.use(express.json());
+
+const atms =  [
+      {
+        id: 1,
+        type: 'Beskontaktni',
+        address: 'Trg Bana Josipa Jelačića 1, Zagreb',
+        coordinates: { E: 15.9770, N: 45.8131 }
+      },
+      {
+        id: 2,
+        type: 'Uplatno-isplatni',
+        address: 'Ilica 10, Zagreb',
+        coordinates: { E: 15.9754, N: 45.8136 }
+      },
+      {
+        id: 3,
+        type: 'Dnevno-noćni trezor',
+        address: 'Vukovarska ulica 269F, Zagreb',
+        coordinates: { E: 16.0076, N: 45.7958 }
+      },
+      {
+        id: 4,
+        type: 'Kovinomat',
+        address: 'Maksimirska cesta 128, Zagreb',
+        coordinates: { E: 16.0055, N: 45.8239 }
+      },
+      {
+        id: 5,
+        type: 'Beskontaktni',
+        address: 'Avenija Dubrovnik 16 (Arena Centar), Zagreb',
+        coordinates: { E: 15.9452, N: 45.7725 }
+      },
+      {
+        id: 6,
+        type: 'Uplatno-isplatni',
+        address: 'Radićeva ul. 2, Zagreb',
+        coordinates: { E: 15.9760, N: 45.8150 }
+      },
+      {
+        id: 7,
+        type: 'Beskontaktni',
+        address: 'Gajeva ul. 5, Zagreb',
+        coordinates: { E: 15.9775, N: 45.8115 }
+      }
+];
+
+app.get("/atms", authenticateToken, (req, res) => {
+    res.json(atms);
+});
+
+app.post("/login", (req, res) => {
+    const { username, password } = req.body;
+    const user = { username, password };
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+    res.json({ accessToken: accessToken });
+});
+
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    if (token == null) return res.sendStatus(401);
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403);
+        req.user = user;
+        next();
+    });
+}
+
+app.listen(3000);
